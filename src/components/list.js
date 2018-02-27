a5345cafb43b08effcd4bfce9f1110fe0933cebe
Irinkes <irinkes@gmail.com>
2018-02-27 10:55:47 +0300

    added component list.js

---
+++ src/components/list.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class List extends React.Component {
+	render() {
+		return (
+		<div class="todo-list">
+			<label><input type="checkbox"/>Do something</label>
+		</div>
+		);
+	}
+}
+
+export default List;
+
